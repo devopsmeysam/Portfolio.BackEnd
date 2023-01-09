a4c0ad2d7d1d95213c04da9816bae1dbc5021a27
devopsmeysam <91230909+devopsmeysam@users.noreply.github.com>
2023-01-09 09:48:51 -0500

    Implements ENV

--- config/config.js
+++ config/config.js
@@ -1,9 +1,7 @@
-//Do not expose your connection credentials!
+require('dotenv').config();
 
 module.exports = {
-    "ATLASDB": "mongodb+srv://meysamdbuser:gwqUiKclkg2K4utn@cluster005.ye5t026.mongodb.net/products",
+    "ATLASDB": process.env.ATLASDB,
     "LOCALDB": "mongodb://localhost:27017/dbapp",
-    "SECRETKEY": "y$B&E)H+MbQeThWmZq4t7w!z%C*F-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s"
+    "SECRETKEY": process.env.SECRETKEY
 }
-
-//T8L1X2HGdkAUGon5nDSmPugbAYP1dRR4jYqV0lkalJNsg3OFJdmUjn8CK1LujDj81ftHrQdNezo8e8pEWlH6eZqCjnm77SaxXdUKak2uBHt8eSMR3kr56V1aF5iqxsUv
--- controllers/inventory.controller.js
+++ controllers/inventory.controller.js
@@ -3,6 +3,7 @@ Student Name: Meysam Mahdavikhansari
 Student ID: 301248106
 Date: Thursday, October 18th, 2022 */
 
+const { framework } = require('passport');
 let Inventory = require('../models/inventory.model');
 
 function getErrorMessage(err) {
@@ -46,126 +47,155 @@ exports.inventoryList = async function (req, res, next) {
 
 
 module.exports.processEdit = (req, res, next) => {
-    let id = req.params.id
+    
+    try {
+        
+        let id = req.params.id
+        
+        let updatedItem = Inventory({
+            _id: id,
+            item: req.body.item,
+            qty: req.body.qty,
+            status: req.body.status,
+            size : {
+                h: req.body.size.h,
+                w: req.body.size.w,
+                uom: req.body.size.uom,
+            },
+            tags: (req.body.tags == "" || req.body.tags == null) ? "" : req.body.tags.split(",").map(word => word.trim()),
+            owner: (req.body.owner == null || req.body.owner == "") ? req.payload.id : req.body.owner
+        });
+        
+        // console.log(updatedItem);
+        
+        Inventory.updateOne({_id: id}, updatedItem, (err, result) => {
+            console.log(err, result);
+            if(err || result.modifiedCount == 0)
+            {
+                console.log(err);
+                // res.end(err);
+                res.status(400).json(
+                    {
+                        success: false,
+                        message: err ? getErrorMessage(err) : "You did NOT change anything! Change a property and then try again."
+                    }
+                    );
+            }
+            else
+            {
+                // console.log(req.body);
+                // refresh the list
+                // res.redirect('/inventory/list');
+                res.status(200).json(
+                    {
+                        success: true,
+                        message: "Item updated successfully."
+                    }
+                    );
+            }
+        });
 
-    let updatedItem = Inventory({
-        _id: id,
-        item: req.body.item,
-        qty: req.body.qty,
-        status: req.body.status,
-        size : {
-            h: req.body.size.h,
-            w: req.body.size.w,
-            uom: req.body.size.uom,
-        },
-        tags: (req.body.tags == "" || req.body.tags == null) ? "" : req.body.tags.split(",").map(word => word.trim()),
-        owner: (req.body.owner == null || req.body.owner == "") ? req.payload.id : req.body.owner
-    });
-
-    // console.log(updatedItem);
-
-    Inventory.updateOne({_id: id}, updatedItem, (err, result) => {
-        console.log(err, result);
-        if(err || result.modifiedCount == 0)
-        {
-            console.log(err);
-            // res.end(err);
-            res.status(400).json(
-                {
-                  success: false,
-                  message: err ? getErrorMessage(err) : "You did NOT change anything! Change a property and then try again."
-                }
-            );
-        }
-        else
-        {
-            // console.log(req.body);
-            // refresh the book list
-            // res.redirect('/inventory/list');
-            res.status(200).json(
-                {
-                  success: true,
-                  message: "Item updated successfully."
-                }
-            );
-        }
-    });
-}
-
-
+    } catch (error) {
+        return res.status(400).json(
+            {
+                success: false,
+                message: getErrorMessage(error)
+            }
+        );
+    }
+}  
 
 
 module.exports.processAdd = (req, res, next) => {
-    let newItem = Inventory({
-        _id: req.body.id,
-        item: req.body.item,
-        qty: req.body.qty,
-        status: req.body.status,
-        size : {
-            h: req.body.size.h,
-            w: req.body.size.w,
-            uom: req.body.size.uom,
-        },
-        tags: (req.body.tags == "" || req.body.tags == null) ? "" : req.body.tags.split(",").map(word => word.trim()),
-        owner: (req.body.owner == null || req.body.owner == "") ? req.payload.id : req.body.owner
-    });
+    
+    try {
 
-    Inventory.create(newItem, (err, item) =>{
-        if(err)
-        {
-            console.log(err);
-            // res.end(err);
-            res.status(400).json(
-                {
-                  success: false,
-                  message: getErrorMessage(err)
-                }
-            );
-        }
-        else
-        {
-            // refresh the book list
-            console.log(item);
-            // res.redirect('/inventory/list');
-            res.status(200).json(
-                {
-                  success: true,
-                  message: "Item added successfully."
-                }
-            );
-        }
-    });
-
+        let newItem = Inventory({
+            _id: req.body.id,
+            item: req.body.item,
+            qty: req.body.qty,
+            status: req.body.status,
+            size : {
+                h: req.body.size.h,
+                w: req.body.size.w,
+                uom: req.body.size.uom,
+            },
+            tags: (req.body.tags == "" || req.body.tags == null) ? "" : req.body.tags.split(",").map(word => word.trim()),
+            owner: (req.body.owner == null || req.body.owner == "") ? req.payload.id : req.body.owner
+        });
+        
+        Inventory.create(newItem, (err, item) =>{
+            if(err)
+            {
+                console.log(err);
+                // res.end(err);
+                res.status(400).json(
+                    {
+                        success: false,
+                        message: getErrorMessage(err)
+                    }
+                );
+            }
+            else
+            {
+                // refresh the book list
+                console.log(item);
+                // res.redirect('/inventory/list');
+                res.status(200).json(item);
+            }
+        });
+                
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(
+            {
+                success: false,
+                message: getErrorMessage(error)
+            }
+        );
+    }
 }
 
 
 
 module.exports.performDelete = (req, res, next) => {
-    let id = req.params.id;
+    
+    try {
 
-    Inventory.findByIdAndRemove({_id: id}, {rawResult: true}, (err, result) => {
-        console.log(err, result);
-        if(err || result.value == null)
-        {
-            console.log(err);
-            // res.end(err);
-            res.status(400).json(
-                {
-                  success: false,
-                  message: err ? getErrorMessage(err) : "Item NOT Found!"
-                }
-            );
-        }
-        else
-        {
-            // refresh the book list
-            // res.redirect('/inventory/list');
-            res.status(200).json(
-                {
-                  success: true,
-                  message: "Item deleted successfully."
-                }
-            );
-        }
-    });
+        let id = req.params.id;
+
+        Inventory.findByIdAndRemove({_id: id}, {rawResult: true}, (err, result) => {
+            console.log(err, result);
+            if(err || result.value == null)
+            {
+                console.log(err);
+                // res.end(err);
+                res.status(400).json(
+                    {
+                      success: false,
+                      message: err ? getErrorMessage(err) : "Item NOT Found!"
+                    }
+                );
+            }
+            else
+            {
+                // refresh the book list
+                // res.redirect('/inventory/list');
+                res.status(200).json(
+                    {
+                      success: true,
+                      message: "Item deleted successfully."
+                    }
+                );
+            }
+        });
+
+    } catch (error) {
+        return res.status(400).json(
+            {
+                success: false,
+                message: getErrorMessage(error)
+            }
+        );
+    }
 }
